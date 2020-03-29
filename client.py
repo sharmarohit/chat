2d4bfbec5f31eda49e7fdf7ca71d12587b7ab2c9
Rohit Sharma <rohit_sh1@yahoo.ca>
2020-03-28 19:58:15 -0700

    dual way chat

--- client.py
+++ client.py
@@ -1,13 +1,18 @@
 import socket
 
 MAX_SIZE_BYTES = 65535
+host = '127.0.0.1'
+port = 3006
 
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#print('The OS assigned the address {} to me'.format(clientsocket.getsockname()))
-message = input('Input message in lowercase:')
-data = message.encode('ascii')
-clientsocket.sendto(data, ('127.0.0.1', 3006))
 
-data, address = clientsocket.recvfrom(MAX_SIZE_BYTES)
-text = data.decode('ascii')
-print('The server {} replied with {!r}'.format(address, text))
+clientsocket.connect((host, port))
+
+while True:
+    message = input('Input message in lowercase:')
+    data = message.encode('ascii')
+    clientsocket.send(data)
+
+    data = clientsocket.recv(MAX_SIZE_BYTES)
+    text = data.decode('ascii')
+    print('The server replied with {!r}'.format(text))
